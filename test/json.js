babb6bc864db7b8e9f8bd3a8ba5dcb2ec1823271
wldo573 <wldo573@test.com>
2021-05-14 16:35:16 +0900

    7교시 커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e1269f6ffc29574d8a839",
+      "age": 24,
+      "eyeColor": "green",
+      "name": "Cindy Richardson"
+    },
+    {
+      "_id": "609e12694cd7159a75520a4d",
+      "age": 31,
+      "eyeColor": "blue",
+      "name": "Kristi Howell"
+    },
+    {
+      "_id": "609e1269248b61b29c0ed69f",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Allyson Clark"
+    },
+    {
+      "_id": "609e12696f019d16288102cc",
+      "age": 31,
+      "eyeColor": "brown",
+      "name": "Hughes Farley"
+    },
+    {
+      "_id": "609e126943dbc903cf2add6d",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Vanessa Fulton"
+    },
+    {
+      "_id": "609e1269dec754631d7e45fc",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Bertie Gillespie"
+    },
+    {
+      "_id": "609e12693d91e02f9c7efa03",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Beulah Cervantes"
+    },
+    {
+      "_id": "609e12694b0a86e97603402e",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Lela Erickson"
+    },
+    {
+      "_id": "609e12699ae8a2b55c6a2720",
+      "age": 29,
+      "eyeColor": "brown",
+      "name": "Dominique Pittman"
+    },
+    {
+      "_id": "609e12690ad6e011af62fd37",
+      "age": 25,
+      "eyeColor": "green",
+      "name": "Imelda Sandoval"
+    },
+    {
+      "_id": "609e1269248cbe5b0c04d1a8",
+      "age": 27,
+      "eyeColor": "blue",
+      "name": "Rhoda Jennings"
+    },
+    {
+      "_id": "609e1269f41e71af21def32a",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Lilly Irwin"
+    },
+    {
+      "_id": "609e12696d9a672490900b6e",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Santana Deleon"
+    },
+    {
+      "_id": "609e12695441748cf33a1c8f",
+      "age": 22,
+      "eyeColor": "green",
+      "name": "Mcdaniel Hogan"
+    },
+    {
+      "_id": "609e1269f259b1614b70c762",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Tania Gallagher"
+    },
+    {
+      "_id": "609e12698df93b57f5701c44",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Renee Vang"
+    },
+    {
+      "_id": "609e12691b6b7407a5dc7f2c",
+      "age": 31,
+      "eyeColor": "blue",
+      "name": "York Dickerson"
+    },
+    {
+      "_id": "609e126909ab03d154234010",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Daphne Tucker"
+    },
+    {
+      "_id": "609e126969043f13453ee675",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Dixie Harrison"
+    },
+    {
+      "_id": "609e126965053403b4c9aa51",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Terrell Dillon"
+    }
+  ]
